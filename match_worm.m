de9a126661719c6aaeefd01ab624b2e71bb38fa8
Andrew Lilja <andrewlilja@gmail.com>
2014-04-20 22:12:26 -0500

    rendering and matching complete, time to fix some bugs

--- match_worm.m
+++ match_worm.m
@@ -11,43 +11,7 @@ start_pos = [10 10];
 width = 20;
 length = 80;
 
-step = length/num_segments;
-
-
 system = ParticleSystem();
-
-% create the mesh
-for(i = 1:num_segments)
-    % create NODES and SPRINGS
-    left_edge  = Node(i*3 - 2, start_pos + [-width/2+rand*5 step*i],[0 0],10,0.5,1);
-    spine      = Node(i*3 - 1, start_pos + [0 step*i],[0 0],10, 0.5);
-    right_edge = Node(i*3, start_pos + [width/2-rand*5 step*i],[0 0],10, 0.5, 1);
-    % a node is spine %
-    k = 3;
-    damp = 0.5;
-    spring_id_base = system.num_springs;
-    left_spring  = Spring(spring_id_base + 1, width/2, k, damp, spine, left_edge);
-    right_spring = Spring(spring_id_base + 2, width/2, k, damp, spine, right_edge);
-    if(i > 1)
-        % a is previous node
-        left_connector  = Spring(spring_id_base + 3, step, k, damp, system.NODES((i-1)*3 - 2), left_edge);
-        spine_connector = Spring(spring_id_base + 4, step, k, damp, system.NODES((i-1)*3 - 1), spine);
-        right_connector = Spring(spring_id_base + 5, step, k, damp, system.NODES((i-1)*3), right_edge);
-    end
-    
-   % populate lists
-    system.add_node(left_edge);
-    system.add_node(spine);
-    system.add_node(right_edge);
-    
-    system.add_spring(left_spring);
-    system.add_spring(right_spring);
-    if(i > 1)
-        system.add_spring(left_connector);
-        system.add_spring(spine_connector);
-        system.add_spring(right_connector);
-    end
-end
 
 %figure; hold on;
 % process image
@@ -75,15 +39,55 @@ for(row = 1:image_size(1))
     end
 end
 
-% now randomly sample each left/right edge pair and use them to create the
-% mesh
+% now randomly sample each left/right edge pair
+k = randperm(size(left_edges,2)); 
+selected_left = left_edges(k(1:num_segments));
+selected_right = right_edges(k(1:num_segments));
 
-%%% IT WILL BE DONE %%%
+% create the mesh
+step = length/num_segments;
+for(i = 1:num_segments)
+    % create NODES and SPRINGS
+    left_edge  = Node(i*3 - 2, start_pos + [selected_left(i).x selected_left(i).y],[0 0],10,0.5,1);
+    spine      = Node(i*3 - 1, start_pos + [0 0],[0 0],10, 0.5);
+    right_edge = Node(i*3, start_pos + [selected_right(i).x selected_right(i).y],[0 0],10, 0.5, 1);
+    % a node is spine %
+    k = 3;
+    damp = 0.5;
+    spring_id_base = system.num_springs;
+    left_spring  = Spring(spring_id_base + 1, width/2, k, damp, spine, left_edge);
+    right_spring = Spring(spring_id_base + 2, width/2, k, damp, spine, right_edge);
+    if(i > 1)
+        % a is previous node
+        left_connector  = Spring(spring_id_base + 3, step, k, damp, system.NODES((i-1)*3 - 2), left_edge);
+        spine_connector = Spring(spring_id_base + 4, step, k, damp, system.NODES((i-1)*3 - 1), spine);
+        right_connector = Spring(spring_id_base + 5, step, k, damp, system.NODES((i-1)*3), right_edge);
+    end
+    
+   % populate lists
+    system.add_node(left_edge);
+    system.add_node(spine);
+    system.add_node(right_edge);
+    
+    system.add_spring(left_spring);
+    system.add_spring(right_spring);
+    if(i > 1)
+        system.add_spring(left_connector);
+        system.add_spring(spine_connector);
+        system.add_spring(right_connector);
+    end
+end
 
 % process
-%for(iteration = 1:30)
-%    system.tick();
-%end
+for(iteration = 1:30)
+    system.tick();
+end
+
+% appears mirrored across a -1 45° line??
 
 % render
-%circle(system.NODES(i).position(1), system.NODES(i).position(2), 1);
+imshow(edges);
+hold on;
+for(i = 1:system.num_nodes)
+    circle(system.NODES(i).position(1), system.NODES(i).position(2), 10);
+end
