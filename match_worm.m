a609716e3efb3ad9316c70fa2a1c3bf94b46380e
Andrew Lilja <andrewlilja@gmail.com>
2014-04-25 16:45:16 -0500

    complete re-write

--- match_worm.m
+++ match_worm.m
@@ -1,7 +1,3 @@
-% TO DO
-% 1. work on sorting nodes by location
-% 2. add video
-
 %%%%%%%%%%%
 % OPTIONS %
 %%%%%%%%%%%
@@ -11,7 +7,7 @@ num_ticks = 20;    % number of times the soft body model should run
 
 % Display Options
 show_normals = 1;        % show edge normals
-show_ribs = 1;           % show node placement ribs
+show_ribs = 0;           % show node placement ribs
 show_node_alignment = 0; % show nodes during each tick of model
 
 % Video Options
@@ -19,168 +15,20 @@ vid = VideoReader('shisto.avi');
 start_frame = 2;
 vid_scale = 0.50;
 
+% just measure length of the normal: if it's very very short, then flip it
+% around
+% alternatively, figure out if we're inside the worm or not. if we are,
+% great, otherwise flip it around
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%% HERE THERE BE DRAGONS %%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% calculate options, pseudo-bitwise
-show_stuff = show_normals + show_ribs + show_node_alignment;
-
-% process image
 first_frame = imresize(read(vid,start_frame),vid_scale);
 mask = roipoly(first_frame);
-background = first_frame;
-for i = 1:3
-    background(:,:,i) = roifill(first_frame(:,:,i),mask);
-end
+
 %%
-% set up soft body and logger
-system = ParticleSystem();
-log = Logger('test.log',0,1);
-num_segments = 10;
-
-% get binary worm fill
-tgt = rgb2gray(background - first_frame);
-target = process_worm(tgt);
-[edges, tresh, gv, gh] = edge(target,'sobel');
-skel = bwmorph(target, 'skel', Inf);
-skel = imdilate(skel, ones(2));
-
-% find edges
-edge_dirs = atan2(gv, gh);
-edge_dirs = edge_dirs(edge_dirs ~= 0);
-[edge_row edge_col] = find(edges);
-
-% calculate normals and find opposite edges
-a_edges = [];
-b_edges = [];
-centerlines = [];
-dist = max(max(bwdist(~target),[],1));
-if(show_stuff)
-    imshow(edges + skel);
-    hold on;
-end
-
-mod_factor = ceil(length(edge_row)/num_segments)
-
-for(i = 1:length(edge_row))
-    if(mod(i,mod_factor) == 0)
-        x1 = edge_row(i);
-        y1 = edge_col(i);
-        dir = atan2(gv(x1,y1), gh(x1,y1));
-        
-        normal = NaN;
-        found_center = 0;
-        for(n = 1:round(dist*2.5))
-            x = round(x1 + cos(dir+pi)*n);
-            y = round(y1 + sin(dir+pi)*n);
-            if(isnan(normal))
-                if(target(x, y) == 0)
-                    normal = [x y];
-                    a_edges = [a_edges Edge(x1, y1)];
-                    b_edges = [b_edges Edge(x, y)];
-                    if(show_ribs)
-                        plot([y1 y],[x1 x],'r');
-                    end
-                end
-            end
-            if(~found_center)
-                if(skel(x, y) == 1)
-                    centerlines = [centerlines Edge(x, y)];
-                    found_center = 1;
-                end
-            end
-        end
-        if(show_normals)
-            plot([y1 y1+sin(pi+dir)*5],[x1 x1+cos(pi+dir)*5]);
-        end
-    end
-end
-
-if(show_stuff)
-    k = waitforbuttonpress();
-end    
-
-% now sample each left/right edge pair
-k = randperm(size(a_edges,2)); 
-num_segments = length(a_edges);
-selected_left = a_edges(1:num_segments);
-selected_right = b_edges(1:num_segments);
-selected_centers = centerlines(1:num_segments);
-
-% create the mesh
-for(i = 1:num_segments)
-    system.num_nodes
-    % create NODES and SPRINGS
-    left_edge  = Node(i*3 - 2, [selected_left(i).i    selected_left(i).j],   [0 0],10, 0.5, 0);
-    spine      = Node(i*3 - 1, [selected_centers(i).i selected_centers(i).j],[0 0],10, 0.5, 0);
-    right_edge = Node(i*3,     [selected_right(i).i    selected_right(i).j], [0 0],10, 0.5, 0);
-    % a node is spine %
-    k = 0.5;
-    damp = 0.1;
-    spring_id_base = system.num_springs;
-    left_spring  = Spring(spring_id_base + 1, 6*vid_scale, k, damp, spine, left_edge);
-    right_spring = Spring(spring_id_base + 2, 6*vid_scale, k, damp, spine, right_edge);
-    if(i > 1)
-        % a is previous node
-        left_connector  = Spring(spring_id_base + 3, 2, k, damp, system.NODES((i-1)*3 - 2), left_edge);
-        spine_connector = Spring(spring_id_base + 4, 2, k, damp, system.NODES((i-1)*3 - 1), spine);
-        right_connector = Spring(spring_id_base + 5, 2, k, damp, system.NODES((i-1)*3), right_edge);
-    end
-    
-   % populate lists
-    system.add_node(left_edge);
-    system.num_nodes
-    system.add_node(spine);
-    system.add_node(right_edge);
-    
-    system.add_spring(left_spring);
-    system.add_spring(right_spring);
-    if(i > 1)
-        system.add_spring(left_connector);
-        system.add_spring(spine_connector);
-        system.add_spring(right_connector);
-    end
-end
-
-if(show_node_alignment)
-    hold off;
-    imshow(edges);
-    hold on;
-end
-
-% process
-for(iteration = 1:num_ticks)
-    iteration
-    system.tick();
-    log.error(num2str(iteration));
-    for(i = 1:system.num_nodes)
-        log.warning(num2str(system.NODES(i).id));
-        log.warning(num2str(system.NODES(i).position));
-        if(show_node_alignment)
-            circle(system.NODES(i).position(1), system.NODES(i).position(2), 5);
-            pause(0.1);
-        end
-    end
-    log.note('-----------------------------');
-end
-
-% render
-hold off;
-imshow(edges);
-hold on;
-
-for(i = 1:system.num_nodes)
-    this_node = system.NODES(i);
-    circle(this_node.position(1), this_node.position(2), 3);
-    log.warning(num2str(this_node.id));
-    log.warning(num2str(this_node.position));
-end
-
-k = waitforbuttonpress();
-%%
-for(frame_num = 1:vid.NumberOfFrames)
+for frame_num = 65:vid.NumberOfFrames
     frame_num
     frame = imresize(read(vid,frame_num),vid_scale);
     % process image
@@ -188,46 +36,45 @@ for(frame_num = 1:vid.NumberOfFrames)
     for i = 1:3
         background(:,:,i) = roifill(frame(:,:,i),mask);
     end
-
-    imshow(edges);
+    
+    %find bw
+    sub2 = uint8(abs(double(background) - double(frame)));
+    sub2 = rgb2gray(sub2);
+    bin2 = sub2 > 10;
+    bin2 = bwareaopen(bin2,50);
+    average_width = 7;
+    
+    % find skel
+    dist_trans1 = bwdist(~imfill(bin2,'holes'));
+    dist_trans = bwdist(~bin2);
+    dist_trans1 = (dist_trans1>average_width*.8)&(dist_trans1<average_width*1.5);
+    dist_trans = bwmorph(bwmorph((dist_trans>average_width*.8)&(dist_trans<average_width*1.5),'thin','inf'),'dilate',1);
+    dist_trans = dist_trans|dist_trans1;
+     
+    edges = edge(bin2,'canny',.1);
+    blob = imfill(bwmorph(edges,'dilate',2),'holes');
+    skel = bwmorph(blob,'thin', inf);
+    
+    branches = bwmorph(skel,'endpoints');
+    [branch_x branch_y] = ind2sub(size(branches), find(branches));
+    
+    geodesic = find(~isnan(bwdistgeodesic(skel, branch_x(1), branch_y(2))));
+    
+    [geo_x geo_y] = ind2sub(size(bin2), geodesic);
+    
+    attachment_points = skel_handles_pixels([geo_x geo_y], num_segments);
+    
+    %imshow(skel + edge(blob));
+    imshow(rgb2gray(frame) + uint8(bin2)* 50);
     hold on;
-    % get binary worm fill
-    tgt = rgb2gray(background - frame);
-    target = process_worm(tgt);
-    edges = edge(target);  
-    [edge_row edge_col] = find(edges);    
-    for(i = 1:length(edge_row))
-        if(mod(i,mod_factor) == 0)
-            x1 = edge_row(i);
-            y1 = edge_col(i);
-            dir = atan2(gv(x1,y1), gh(x1,y1));
-
-            normal = NaN;
-            for(n = 1:round(dist*2.5))
-                x = round(x1 + cos(dir+pi)*n);
-                y = round(y1 + sin(dir+pi)*n);
-                if(isnan(normal))
-                    if(target(x, y) == 0)
-                        normal = [x y]
-                        a_edges = [a_edges Edge(x1, y1)];
-                        b_edges = [b_edges Edge(x, y)];
-                        plot([y1 y],[x1 x],'r');
-                    end
-                end
-            end
-        end
+    for i = 1:length(attachment_points)
+        circle(attachment_points(i,2),attachment_points(i,1),5);
     end
     
-    
-    for(i = 1:num_segments)
-        system.NODES(i*3+2).position = a_edges(i).pos;
-        system.NODES(i*3).position = b_edges(i).pos;
-    end
-    
-    %for(i = 1:system.num_nodes)
-    %    this_node = system.NODES(i);
-    %    circle(this_node.position(1), this_node.position(2), 3);
-    %end
+    diff = [attachment_points(end,1) - attachment_points(1,1); ...
+            attachment_points(end,2) - attachment_points(1,2)];
+    dir = atan2(diff(1),diff(2));
+    plot([50 50+50*cos(dir)],[50 50+50*sin(dir)])
     
     pause(0.01);
 end
--- test.m
+++ test.m
@@ -1,55 +1,44 @@
-num_frames = 500;
-
-num_segments = 20;
-start_pos = [10 10];
-width = 20;
-length = 80;
-
-step = length/num_segments;
-
-
-system = ParticleSystem();
-
-% create the mesh
-for(i = 1:num_segments)
-    % create NODES and SPRINGS
-    left_edge  = Node(i*3 - 2, start_pos + [-width/2+rand*5 step*i],[0 0],10,0.5,1);
-    spine      = Node(i*3 - 1, start_pos + [0 step*i],[0 0],10, 0.5);
-    right_edge = Node(i*3, start_pos + [width/2-rand*5 step*i],[0 0],10, 0.5, 1);
-    % a node is spine %
-    k = 3;
-    damp = 0.5;
-    spring_id_base = system.num_springs;
-    left_spring  = Spring(spring_id_base + 1, width/2, k, damp, spine, left_edge);
-    right_spring = Spring(spring_id_base + 2, width/2, k, damp, spine, right_edge);
-    if(i > 1)
-        % a is previous node
-        left_connector  = Spring(spring_id_base + 3, step, k, damp, system.NODES((i-1)*3 - 2), left_edge);
-        spine_connector = Spring(spring_id_base + 4, step, k, damp, system.NODES((i-1)*3 - 1), spine);
-        right_connector = Spring(spring_id_base + 5, step, k, damp, system.NODES((i-1)*3), right_edge);
-    end
-    
-   % populate lists
-    system.add_node(left_edge);
-    system.add_node(spine);
-    system.add_node(right_edge);
-    
-    system.add_spring(left_spring);
-    system.add_spring(right_spring);
-    if(i > 1)
-        system.add_spring(left_connector);
-        system.add_spring(spine_connector);
-        system.add_spring(right_connector);
-    end
+clc
+close all
+clear all
+%first read in the video
+[file,path] = uigetfile('*.mp4');
+filename = [path,file];
+vid = VideoReader(filename);
+%%
+%display the first frame to perform segmentation
+start_frame = 100;
+first_frame = read(vid,start_frame);
+mask = roipoly(first_frame);
+background = first_frame;
+for i = 1:3
+    background(:,:,i) = roifill(first_frame(:,:,i),mask);
 end
-
-figure; hold on;
-
-for(frame_num = 1:num_frames)
-    frame_num
-    for(i = 1:system.num_nodes)
-        circle(system.NODES(i).position(1), system.NODES(i).position(2), 1);
-    end
-    pause(0.1)
-    system.tick();
+%%
+%use "optical flow" to decide which sections of the worm need to be updated
+flow_image = zeros(size(first_frame,1),size(first_frame,2),3);
+average_width=50;
+for i = start_frame:start_frame+30;
+    %find binary worm for the previous frame
+    sub = uint8(abs(double(background) - double(first_frame)));
+    sub = rgb2gray(sub);
+    bin1 = sub > 10;
+    bin1=~bwareaopen(~bin1,50,4);
+    
+    %fine binary worm for the current frame
+    second_frame = read(vid,i);
+    sub2 = uint8(abs(double(background) - double(second_frame)));
+    flow_image = double(second_frame)/255;
+    sub2 = rgb2gray(sub2);
+    bin2 = sub2 > 10;
+    bin2=~bwareaopen(~bin2,50);
+    
+    %develop skeleton for the current frame
+    dist_trans1 = bwdist(~imfill(bin2,'holes'));
+    dist_trans = bwdist(~bin2);
+    dist_trans1 = (dist_trans1>average_width*.8)&(dist_trans1<average_width*1.5);
+    dist_trans = bwmorph(bwmorph((dist_trans>average_width*.8)&(dist_trans<average_width*1.5),'thin','inf'),'dilate',1);
+    dist_trans = dist_trans|dist_trans1;
+    
+    skel = bwmorph(dist_trans,'thin',inf);
 end
